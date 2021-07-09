5e5ace15be582b0dacbafb6e12a36877b62e930a
onthepeakofnormal <gius_martin@yahoo.de>
2021-07-09 11:30:16 +0200

    introduce environment to make code cleaner

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,30 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 import       Language.Haskell.Interpreter as Hint
 import       Language.Haskell.Interpreter.Unsafe as Hint
 import       Sound.Tidal.Context as T
 import       Control.Concurrent.MVar
 import       Control.Concurrent
 import Control.Monad (void)
+import Control.Monad.Reader
 import Sound.Tidal.Stream (Target(..))
 import Text.Parsec(parse)
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core as C hiding (text)
+-- import Graphics.UI.Threepenny.Internal(unUI)
+
 
 import Parse
+
+data Env = Env {window :: Window
+               ,stream :: Stream
+               ,input :: Element
+               ,output :: Element
+               ,errors :: Element
+               ,strKey :: MVar Bool
+               ,enterKey :: MVar Bool
+               }
 
 libs = [
     "Sound.Tidal.Context"
@@ -87,68 +101,90 @@ setup stream win = void $ do
       output <- UI.div #+ [ string "output goes here" ]
       errors <- UI.div #+ [ string "errors go here" ]
 
-
       strKey <- liftIO $ newMVar False
       enterKey <- liftIO $ newMVar False
 
+      let env = Env win stream input output errors strKey enterKey
+
       body <- UI.getBody win
-      on UI.keydown body $ \x -> keydownEvent strKey enterKey x input errors output
-      on UI.keyup body $ \x -> liftIO $ keyupEvent strKey enterKey x
+      on UI.keydown body $ \x -> liftIO $ runReaderT (keydownEvent x) env
+      on UI.keyup body $ \x -> liftIO $ runReaderT (keyupEvent x) env
 
       script <- mkElement "script"
                   C.# C.set UI.text "const codemirrorEditor = CodeMirror.fromTextArea(document.getElementById('code'), {lineNumbers: true, mode: \"haskell\"});"
 
       UI.getBody win #+ [element input, element script, element errors, element output]
-      where keydownEvent strKey enterKey x input errors output = do
-                                          case x of
-                                              17 -> do
-                                                liftIO $ takeMVar strKey
-                                                liftIO $ putMVar strKey True
-                                                enter <- liftIO $ readMVar enterKey
-                                                case enter of
-                                                  True ->  interpretC stream input errors output
-                                                  _ -> return ()
-                                              13 -> do
-                                                liftIO $ takeMVar enterKey
-                                                liftIO $ putMVar enterKey True
-                                                str <- liftIO $ readMVar strKey
-                                                case str of
-                                                  True -> interpretC stream input errors output
-                                                  _ -> return ()
-                                              _ -> return ()
 
-keyupEvent :: MVar Bool -> MVar Bool -> Int -> IO ()
-keyupEvent strKey enterKey x = do
+instance MonadUI (ReaderT Env IO) where
+  liftUI m = do
+            env <- ask
+            let win = window env
+            liftIO $ runUI win m
+
+keydownEvent :: Int -> ReaderT Env IO ()
+keydownEvent x = do
+            env <- ask
+            let str = strKey env
+                enter = enterKey env
+            case x of
+                17 -> do
+                  liftIO $ takeMVar str
+                  liftIO $ putMVar str True
+                  enter <- liftIO $ readMVar enter
+                  case enter of
+                    True -> do
+                          liftIO $ putStrLn "hey"
+                          interpretC
+                    _ -> return ()
+                13 -> do
+                  liftIO $ takeMVar enter
+                  liftIO $ putMVar enter True
+                  str <- liftIO $ readMVar str
+                  case str of
+                    True -> interpretC
+                    _ -> return ()
+                _ -> return ()
+
+keyupEvent :: Int -> ReaderT Env IO ()
+keyupEvent x = do
+              env <- ask
+              let str = strKey env
+                  enter = enterKey env
               case x of
                   17 -> do
-                    takeMVar strKey
-                    putMVar strKey False
+                    liftIO $ takeMVar str
+                    liftIO $ putMVar str False
                   13 -> do
-                    takeMVar enterKey
-                    putMVar enterKey False
+                    liftIO $ takeMVar enter
+                    liftIO $ putMVar enter False
                   _ -> return ()
 
-interpretC :: Stream -> Element -> Element -> Element -> UI ()
-interpretC stream input errors output = do
-                contents <- C.get editorValue input
-                let parsed = parse parseCommand "" contents
-                    p = streamReplace stream
-                case parsed of
-                  Left err -> do
-                        element errors C.# C.set UI.text ( "Parse Error:" ++ show err )
-                        return ()
-                  Right command -> case command of
-                                          (D num str) -> do
-                                                  res <- liftIO $ Hint.runInterpreter $ do
-                                                      Hint.set [languageExtensions := exts]
-                                                      Hint.setImports libs
-                                                      Hint.interpret str (Hint.as :: ControlPattern)
-                                                  case res of
-                                                      Right pat -> do
-                                                        element output C.# C.set UI.text ( "control pattern:" ++ show pat )
-                                                        liftIO $ p num $ pat |< orbit (pure $ num-1)
-                                                      Left  err -> do
-                                                        element errors C.# C.set UI.text ( "Interpreter Error:" ++ show err )
-                                                        return ()
-                                          (Hush)      -> liftIO $ streamHush stream
-                                          (Cps x)   -> liftIO $ streamOnce stream $ cps (pure x)
+interpretC :: ReaderT Env IO ()
+interpretC  = do
+        env <- ask
+        let inp = input env
+            out = output env
+            err = errors env
+            str = stream env
+        contents <- liftUI $ C.get editorValue inp
+        let parsed = parse parseCommand "" contents
+            p = streamReplace str
+        case parsed of
+          Left e -> do
+                liftUI $ element err C.# C.set UI.text ( "Parse Error:" ++ show e )
+                return ()
+          Right command -> case command of
+                                  (D num string) -> do
+                                          res <- liftIO $ Hint.runInterpreter $ do
+                                              Hint.set [languageExtensions := exts]
+                                              Hint.setImports libs
+                                              Hint.interpret string (Hint.as :: ControlPattern)
+                                          case res of
+                                              Right pat -> do
+                                                liftUI $ element out C.# C.set UI.text ( "control pattern:" ++ show pat )
+                                                liftIO $ p num $ pat |< orbit (pure $ num-1)
+                                              Left  e -> do
+                                                liftUI $ element err C.# C.set UI.text ( "Interpreter Error:" ++ show e )
+                                                return ()
+                                  (Hush)      -> liftIO $ streamHush str
+                                  (Cps x)   -> liftIO $ streamOnce str $ cps (pure x)
