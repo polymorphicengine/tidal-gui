4000d065d5c6132f675cb9fdacae3e77e74e039e
onthepeakofnormal <gius_martin@yahoo.de>
2021-07-24 14:23:58 +0200

    automatic brackat matching and closing option

--- static/tidal.js
+++ static/tidal.js
@@ -1,13 +1,13 @@
-function controlLoadFile(){
-
-   var fileToLoad = document.getElementById("fileInput").files[0];
-   let reader = new FileReader();
-
-   reader.readAsText(fileToLoad);
-
-   reader.onload = function() {
-   controlEditor.getDoc().setValue(reader.result);
-   };
+ function controlLoadFile(){
+ 
+    var fileToLoad = document.getElementById("fileInput").files[0];
+    let reader = new FileReader();
+    
+    reader.readAsText(fileToLoad);
+    
+    reader.onload = function() {
+   	controlEditor.getDoc().setValue(reader.result);
+    };
 
 }
 
@@ -16,7 +16,7 @@ function controlSaveFile()
     var textToSave = controlEditor.getValue();
     var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
     var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-
+ 
     var downloadLink = document.createElement("a");
     downloadLink.download = 'untitled.tidal';
     downloadLink.innerHTML = "Download File";
@@ -24,7 +24,7 @@ function controlSaveFile()
     downloadLink.onclick = destroyClickedElement;
     downloadLink.style.display = "none";
     document.body.appendChild(downloadLink);
-
+ 
     downloadLink.click();
 }
 
@@ -35,14 +35,14 @@ function destroyClickedElement(event)
 
 function upFocus(cm){
 	definitionsEditor.focus()
-}
+} 
 
 function downFocus(cm){
 	controlEditor.focus()
 }
-
+	
 function openDocs(cm){
-	var loc = cm.findWordAt(cm.getCursor());
-	var word = cm.getRange(loc.anchor, loc.head);
+	var loc = cm.findWordAt(cm.getCursor()); 
+	var word = cm.getRange(loc.anchor, loc.head); 
 	window.open("https://tidalcycles.org/search?q=" + word,"_blank")
 }
--- static/tidalConfig.js
+++ static/tidalConfig.js
@@ -1,42 +1,47 @@
-const controlExtraKeys = {"Ctrl-.": hush,
-			   "Ctrl-Enter": evaluate,
-			   "Ctrl-Up": upFocus,
-			   "Ctrl-D": openDocs,
-			   "Ctrl-1": mute1,
-			   "Ctrl-2": mute2,
-			   "Ctrl-3": mute3,
-			   "Ctrl-4": mute4,
-			   "Ctrl-5": mute5,
-			   "Ctrl-6": mute6,
-			   "Ctrl-7": mute7,
-			   "Ctrl-8": mute8,
-			   "Ctrl-9": mute9,
-			   "Ctrl-S": controlSaveFile,
-			   "Ctrl-O": controlLoadFile,
+const controlExtraKeys = {"Ctrl-.": hush, 
+			   "Ctrl-Enter": evaluate, 
+			   "Ctrl-Up": upFocus, 
+			   "Ctrl-D": openDocs, 
+			   "Ctrl-1": mute1, 
+			   "Ctrl-2": mute2, 
+			   "Ctrl-3": mute3, 
+			   "Ctrl-4": mute4, 
+			   "Ctrl-5": mute5, 
+			   "Ctrl-6": mute6, 
+			   "Ctrl-7": mute7, 
+			   "Ctrl-8": mute8, 
+			   "Ctrl-9": mute9, 
+			   "Ctrl-S": controlSaveFile, 
+			   "Ctrl-O": controlLoadFile, 
 			   "Ctrl-/": 'toggleComment'}
-
-const definitionsExtraKeys = {"Ctrl-.": hush,
-			"Ctrl-Enter": evaluate,
-			"Ctrl-Down": downFocus,
-			"Ctrl-D": openDocs,
-			"Ctrl-1": mute1,
-			"Ctrl-2": mute2,
-			"Ctrl-3": mute3,
-			"Ctrl-4": mute4,
-			"Ctrl-5": mute5,
-			"Ctrl-6": mute6,
-			"Ctrl-7": mute7,
-			"Ctrl-8": mute8,
-			"Ctrl-9": mute9,
+			   
+const definitionsExtraKeys = {"Ctrl-.": hush, 
+			"Ctrl-Enter": evaluate, 
+			"Ctrl-Down": downFocus, 
+			"Ctrl-D": openDocs, 
+			"Ctrl-1": mute1, 
+			"Ctrl-2": mute2, 
+			"Ctrl-3": mute3, 
+			"Ctrl-4": mute4, 
+			"Ctrl-5": mute5, 
+			"Ctrl-6": mute6, 
+			"Ctrl-7": mute7, 
+			"Ctrl-8": mute8, 
+			"Ctrl-9": mute9, 
 			"Ctrl-/": 'toggleComment'}
 
-const controlEditorSettings = {lineNumbers: true,
-			 	mode: "haskell",
-			 	extraKeys: controlExtraKeys
+const controlEditorSettings = {lineNumbers: true, 
+			 	mode: "haskell", 
+			 	extraKeys: controlExtraKeys,
+			 	matchBrackets: true,
+			 	autoCloseBrackets: true
 			       }
-
-
-const definitionsEditorSettings = {lineNumbers: true,
-			 	    mode: "haskell",
-			 	    extraKeys: definitionsExtraKeys
+			
+			
+const definitionsEditorSettings = {lineNumbers: true, 
+			 	    mode: "haskell", 
+			 	    extraKeys: definitionsExtraKeys,
+			 	    matchBrackets: true,
+			 	    autoCloseBrackets: true
 			 	   }
+			
