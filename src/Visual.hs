d31e320568def0a54dbb057472be3baf4ea79f07
onthepeakofnormal <gius_martin@yahoo.de>
2021-08-13 12:24:50 +0200

    sort by channels and each sample gets random color

--- src/Visual.hs
+++ src/Visual.hs
@@ -2,13 +2,15 @@ module Visual where
 
 import Sound.Tidal.Context as T hiding (mute,solo,(#))
 import Data.List (groupBy)
-import Data.Map (toList)
+import Data.Map as Map (Map,toList,lookup, insert)
 
 import Control.Monad (void)
 import Control.Concurrent (threadDelay)
-import Control.Concurrent.MVar (readMVar)
+import Control.Concurrent.MVar (MVar, readMVar, takeMVar, putMVar)
 import Sound.OSC.FD (time)
 import Sound.Tidal.Tempo (timeToCycles)
+
+import System.Random
 
 import Graphics.UI.Threepenny as UI hiding (map)
 import qualified Graphics.UI.Threepenny.SVG  as SVG
@@ -22,6 +24,10 @@ data VisEvent a = VAt {vStart :: Time
 
 type VisPat a = [VisEvent a]
 
+--map sample names to colors
+--might change first argument from Value to String, so users can write custom color mappings easyily
+type ColorMap = Map Value String
+
 patToVis :: Time -> Pattern a -> VisPat a
 patToVis t pat = map evToVis evs
           where evs = filter eventHasOnset $ queryArc pat (Arc t (t+1))
@@ -30,53 +36,80 @@ evToVis :: T.Event a -> VisEvent a
 evToVis ev = VAt x y (T.value ev)
           where (Arc x y) = part ev
 
-evToSVG :: Int -> Time -> VisEvent a -> UI Element
-evToSVG i t (VAt st en _) = SVG.rect
-                                # set SVG.width wi
-                                # set SVG.x stPos
-                                # set SVG.y yPos
-                                # set SVG.height "25"
-                                # set SVG.stroke "green"
-                                # set SVG.stroke_width "2"
-                                # set SVG.fill "yellow"
-                      where wi = show $ (fromRational (en - st)*400 :: Double)
-                            stPos = show $ (fromRational (st - t)*400 :: Double)
-                            yPos = show $ i*25
+evToSVGControl :: Int -> Time -> MVar ColorMap -> VisEvent ValueMap  -> UI Element
+evToSVGControl i t cMapMV (VAt st en valMap) = case Map.lookup "s" valMap of
+                                              Just samp -> do
+                                                          cM <- liftIO $ takeMVar cMapMV
+                                                          case Map.lookup samp cM of
+                                                            Just col -> do
+                                                                      _ <- liftIO $ putMVar cMapMV cM
+                                                                      SVG.rect
+                                                                            # set SVG.width wi
+                                                                            # set SVG.x stPos
+                                                                            # set SVG.y yPos
+                                                                            # set SVG.height (show heightScale)
+                                                                            # set SVG.stroke "black"
+                                                                            # set SVG.stroke_width "2"
+                                                                            # set SVG.fill col
+                                                            Nothing   -> do
+                                                                      c <- liftIO $ getRandomColor
+                                                                      _ <- liftIO $ putMVar cMapMV (Map.insert samp c cM)
+                                                                      SVG.rect
+                                                                            # set SVG.width wi
+                                                                            # set SVG.x stPos
+                                                                            # set SVG.y yPos
+                                                                            # set SVG.height (show heightScale)
+                                                                            # set SVG.stroke "black"
+                                                                            # set SVG.stroke_width "2"
+                                                                            # set SVG.fill c
+                                              Nothing -> SVG.rect
+                      where wi = show $ (fromRational (en - st)*widthScale :: Double)
+                            stPos = show $ (fromRational (st - t)*widthScale :: Double)
+                            yPos = show $ i*heightScale
+                            widthScale = 400
+                            heightScale = 25
 
+getRandomColor :: IO String
+getRandomColor = do
+            g <- newStdGen
+            colors <- return $ take 3 (randomRs (0, 255) g :: [Int])
+            return $ "rgb(" ++ show (colors!!0) ++ "," ++ show (colors!!1) ++ "," ++ show (colors!!2) ++ ")"
 
 svgHead :: UI Element
 svgHead = SVG.svg
         # set SVG.width "100vw"
-        # set SVG.height "200"
+        # set SVG.height "50vh"
 
-visPatToSVG :: Eq a => Time -> VisPat a -> [UI Element]
-visPatToSVG t = (visPatRec t) . groupByVals
+visPatLine :: Int -> Time -> MVar ColorMap -> VisPat ValueMap -> [UI Element]
+visPatLine i t cMV evs = map (evToSVGControl i t cMV) evs
 
-patToSVG :: Eq a => Time -> Pattern a -> UI Element
-patToSVG t pat = svgHead #+ els
-            where els = ((visPatToSVG t) . (patToVis t)) pat
+visPatRec :: Time -> MVar ColorMap -> [VisPat ValueMap] -> [UI Element]
+visPatRec t cMV vs = concatMap (\(i,e) -> visPatLine i t cMV e) (zip [0..] vs)
 
+patToSVGChan :: Time -> MVar ColorMap  -> [VisPat ValueMap] -> UI Element
+patToSVGChan t cMV ps = svgHead #+ visPatRec t cMV ps
+
+
+--groups by the value, so for a controlpattern groups by the valuemap as a whole
 groupByVals :: Eq a => VisPat a -> [VisPat a]
 groupByVals = groupBy (\x y -> vVal x == vVal y)
 
-visPatLine :: Int -> Time -> VisPat a -> [UI Element]
-visPatLine i t evs = map (evToSVG i t) evs
+--groups by channels (standard)
+groupByChan :: Time -> [ControlPattern] -> [VisPat ValueMap]
+groupByChan t = map (patToVis t)
 
-visPatRec :: Time -> [VisPat a] -> [UI Element]
-visPatRec t vs = concatMap (\(i,e) -> visPatLine i t e) (zip [0..] vs)
-
-
-visualizeStream :: Stream -> UI Element
-visualizeStream str = do
+--get patterns playing and transform them to svg elements
+makeSVG :: Stream -> MVar ColorMap -> UI Element
+makeSVG str cMV = do
             tempo <- liftIO $ readMVar $ sTempoMV str
             pMap <- liftIO $ readMVar $ sPMapMV str
             t <- time
             let c = timeToCycles tempo t
-                pat = stack $ map (pattern . snd) $ toList pMap
-            patToSVG c pat
+                pats = map (pattern . snd) $ toList pMap
+            patToSVGChan c cMV (groupByChan c pats)
 
-visualizeStreamLoop :: Window -> Element -> Stream -> IO ()
-visualizeStreamLoop win svg str = do
-                        void $ runUI win $ do d <- visualizeStream str ; element svg # set UI.children [d]
+visualizeStreamLoop :: Window -> Element -> Stream -> MVar ColorMap -> IO ()
+visualizeStreamLoop win svg str cMapMV = do
+                        void $ runUI win $ do d <- makeSVG str cMapMV ; element svg # set UI.children [d]
                         threadDelay 100000
-                        visualizeStreamLoop win svg str
+                        visualizeStreamLoop win svg str cMapMV
