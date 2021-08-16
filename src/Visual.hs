dbfcad833d7d442561d81bb41691ef2679cc175d
onthepeakofnormal <gius_martin@yahoo.de>
2021-08-16 14:34:25 +0200

    arcs!

--- src/Visual.hs
+++ src/Visual.hs
@@ -10,6 +10,8 @@ import Control.Concurrent.MVar (MVar, readMVar, takeMVar, putMVar)
 import Sound.OSC.FD (time)
 import Sound.Tidal.Tempo (timeToCycles)
 
+import Numeric (showFFloat)
+
 import System.Random
 
 import Graphics.UI.Threepenny as UI hiding (map)
@@ -77,14 +79,15 @@ getRandomColor = do
 
 svgHead :: UI Element
 svgHead = SVG.svg
-        # set SVG.width "100vw"
-        # set SVG.height "50vh"
+            # set SVG.x "50%" -- position relative to window
+            # set SVG.y "60%"
+            # set style [("overflow","visible")]
 
-visPatLine :: Int -> Time -> MVar ColorMap -> VisPat ValueMap -> [UI Element]
-visPatLine i t cMV evs = map (evToSVGControl i t cMV) evs
+visPatLine :: Integer -> Time -> MVar ColorMap -> VisPat ValueMap -> [UI Element]
+visPatLine i t cMV evs = map (evToSVGControlArc i t cMV) evs
 
 visPatRec :: Time -> MVar ColorMap -> [VisPat ValueMap] -> [UI Element]
-visPatRec t cMV vs = concatMap (\(i,e) -> visPatLine i t cMV e) (zip [0..] vs)
+visPatRec t cMV vs = concatMap (\(i,e) -> visPatLine i t cMV e) (zip [5..] vs)
 
 patToSVGChan :: Time -> MVar ColorMap  -> [VisPat ValueMap] -> UI Element
 patToSVGChan t cMV ps = svgHead #+ visPatRec t cMV ps
@@ -108,8 +111,74 @@ makeSVG str cMV = do
                 pats = map pattern $ filter (\x -> not (mute x)) (map snd $ toList pMap)
             patToSVGChan c cMV (groupByChan c pats)
 
+-- doesn't perform good with many elements
 visualizeStreamLoop :: Window -> Element -> Stream -> MVar ColorMap -> IO ()
 visualizeStreamLoop win svg str cMapMV = do
-                        void $ runUI win $ do d <- makeSVG str cMapMV ; element svg # set UI.children [d]
+                        void $ runUI win $ do
+                                    d <- SVG.svg
+                                            # set SVG.width "97vw"
+                                            # set SVG.height "70vh"
+                                            #+ [makeSVG str cMapMV]
+                                    element svg # set UI.children [d]
                         threadDelay 100000
                         visualizeStreamLoop win svg str cMapMV
+
+--------- arcs
+
+makeArcD :: String -> String -> ((String, String), (String, String)) -> String
+makeArcD radius larcflag ((startX, startY), (endX, endY)) = "M " ++ startX ++ " " ++ startY ++
+                                                           " A " ++ radius ++ " " ++ radius ++ " 0 " ++ larcflag ++ " 1 " ++ endX ++ " " ++ endY
+
+angle :: Float -> Float -> Float -> Float
+angle intervalStart intervalEnd inp = 2*pi*(inp - intervalStart) / (intervalEnd - intervalStart)
+
+arcPosition :: Float -> Float -> Float -> Float -> Float -> ((String,String),(String,String))
+arcPosition intervalStart intervalEnd startPos endPos r = ((startPosArcX, startPosArcY), (endPosArcX, endPosArcY))
+                                                        where startPosArcX = (showFFloat (Just 2) $ r*(cos $ startAngle )) ""
+                                                              startPosArcY = (showFFloat (Just 2) $ r*(sin $ startAngle ) )  ""
+                                                              endPosArcX = (showFFloat (Just 2) $ r*(cos $ endAngle ))  ""
+                                                              endPosArcY = (showFFloat (Just 2) $ r*(sin $ endAngle ))  ""
+                                                              startAngle = angle intervalStart intervalEnd startPos
+                                                              endAngle = angle intervalStart intervalEnd endPos
+
+
+makeArc :: Float -> Float -> Float -> Float -> Float -> String
+makeArc intervalStart intervalEnd startPos endPos r | endPos - startPos <= intervalEnd / 2 = makeArcD (show r) "0" $ arcPosition intervalStart intervalEnd startPos endPos r  --short way
+                                                    | otherwise = makeArcD (show r) "1" $ arcPosition intervalStart intervalEnd startPos endPos r                             --long way
+
+
+-- each event corresponds to an arc
+evToSVGControlArc :: Integer -> Time -> MVar ColorMap -> VisEvent ValueMap  -> UI Element
+evToSVGControlArc i t cMapMV (VAt st en valMap) =
+                                    case Map.lookup "s" valMap of
+                                             Just (VS samp) -> do
+                                                         cM <- liftIO $ takeMVar cMapMV
+                                                         case Map.lookup samp cM of
+                                                           Just col -> do
+                                                                     _ <- liftIO $ putMVar cMapMV cM
+                                                                     SVG.path
+                                                                           # set SVG.d arcD
+                                                                           # set SVG.stroke col
+                                                                           # set SVG.stroke_width "7"
+                                                                           # set SVG.fill "none"
+                                                                           # set SVG.stroke_linecap "round"
+                                                                           # set style [("filter",brightness)]
+                                                           Nothing   -> do
+                                                                     c <- liftIO $ getRandomColor
+                                                                     _ <- liftIO $ putMVar cMapMV (Map.insert samp c cM)
+                                                                     SVG.path
+                                                                           # set SVG.d arcD
+                                                                           # set SVG.stroke c
+                                                                           # set SVG.stroke_width "7"
+                                                                           # set SVG.fill "none"
+                                                                           # set SVG.stroke_linecap "round"
+                                                                           # set style [("filter",brightness)]
+                                             _ -> SVG.path
+                                   where wi = fromRational (en - st)*widthScale :: Float
+                                         stPos = fromRational (st - t)*widthScale :: Float
+                                         endPos = stPos + wi
+                                         r = fromInteger (i*heightScale) ::Float
+                                         arcD = makeArc 0 widthScale stPos endPos r
+                                         widthScale = 400
+                                         heightScale = 7
+                                         brightness = if (abs (widthScale - stPos) <= 50) then "brightness(2)" else "brightness(1)"
