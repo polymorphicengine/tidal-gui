322d5cc06b71d1416b3462ad08cf41a228f57c70
onthepeakofnormal <gius_martin@yahoo.de>
2021-08-19 08:46:39 +0200

    turn display of by default

--- src/Configure.hs
+++ src/Configure.hs
@@ -118,6 +118,7 @@ bootTidal' = [ "p = streamReplace tidal"
               ,"r = pI \"r\""
               ,"g = pI \"g\""
               ,"b = pI \"b\""
+              ,"display = pS \"display\" \"true\" "
              ]
 
 bootTidal :: String
