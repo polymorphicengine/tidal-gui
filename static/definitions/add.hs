dddf2e7249d50395ab10d5b2145357b4e57e1694
Martin <58735735+polymorphicengine@users.noreply.github.com>
2022-07-16 16:02:39 +0200

    Delete add.hs

--- static/definitions/add.hs
+++
@@ -1,8 +0,0 @@
-let
-_add :: Time -> Pattern a -> Pattern a -> Pattern a
-_add t value pat = slow (pure $ 1+t) $ timeCat [(shift,pat),(1-shift, value)]
-				where shift = 1 / (t + 1)
-add :: Pattern Time -> Pattern a -> Pattern a -> Pattern a
-add pt x y = innerJoin $ fmap (\t -> _add t x y) pt
-
-
