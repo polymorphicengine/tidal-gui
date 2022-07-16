c29fe8dcc911d70192a378f8bd6288069fe43fc5
Martin <58735735+polymorphicengine@users.noreply.github.com>
2022-07-16 16:05:42 +0200

    Delete vec.hs

--- static/definitions/vec.hs
+++
@@ -1,9 +0,0 @@
-let
-vec :: Eq a => (a -> a -> a) -> Pattern a -> Pattern a -> Pattern a
-vec f p1 p2 = uncollect $ do
-                     xs <- collect p1
-                     ys <- collect p2
-                     return $ zipWith f xs ys
---
-(#>) :: (Unionable a, Eq a) => Pattern a -> Pattern a -> Pattern a
-(#>) = vec Sound.Tidal.Context.union
