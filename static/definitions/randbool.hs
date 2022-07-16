db02ed92e2cd5086ce9979c18633ead61234175e
Martin <58735735+polymorphicengine@users.noreply.github.com>
2022-07-16 16:05:22 +0200

    Delete randbool.hs

--- static/definitions/randbool.hs
+++
@@ -1,6 +0,0 @@
-let _wrandbool :: Double -> Pattern Time -> Pattern Bool
-    _wrandbool x n = segment n $ wchoose [(True,x),(False,1-x)]
-    wrandbool = tParam _wrandbool
-    randbool = wrandbool 0.5
-    wrLoop w cycs dens shift = timeLoop cycs $ shift ~> wrandbool w dens
-    rLoop = wrLoop 0.5
